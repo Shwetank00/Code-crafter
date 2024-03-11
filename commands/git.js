9f4b0ee96bf2921ae3b6212d47bdf457a00705d1
Shwetank jain <shwetankjain00@gmail.com>
2024-03-12 00:44:25 +0530

    command added

---
+++ commands/git.js
@@ -0,0 +1,14 @@
+// git init for creating git repo 
+// git status                      to check the working directory and see untracked files.
+// git  add <file name>            to stage a file that you want to track changes on.
+// git commit -m "commit message"  to save the staged content as a new commit in the repository with a log message.
+// git log                         to view the commit history 
+// git add .  (dot)                to stage all new/modified files in the current folder.
+// git diff <file name>            to compare staging area with last committed version.
+// git checkout <file name>        to discard changes made to a file since last commit.
+
+// git remote add  origin         https://github.com/Shwetank00/Code-crafter   # Adds a remote repository for push/pull operations
+// git push -u  origin main       To push your local repository upstream to GitHub
+
+// git rm --cached -r .               to remore staged files 
+// .gitignore                         to ignore the files add their name in this folder 
---
+++ commands/javascript.js
@@ -0,0 +1,49 @@
+// for learning concatenation "+" in js
+// var message="hello"
+// var yourname= prompt("enter your name");
+// alert(message + " " + yourname);
+
+// for learning length of string in js
+// var input =prompt("enter your string");
+// var len=input.length;
+// var remain=140-len;
+// alert("you have written " +len+ " characters, you have "+remain+" characters left");
+
+// you need "()" to calculate anything inside alert
+// var input =prompt("enter your string");
+// alert("you have written " +(input.length)+ " characters, you have "+(140-input.length)+" characters left");
+
+// for learning slicing in js
+// var tweet=prompt("Enter your tweet");
+// var tweetunder140=tweet.slice(0,140);
+// alert(tweetunder140);
+
+// alert(prompt("enter your tweet:").slice(0,140));
+
+// case changing
+// var name=prompt("enter your name");
+// name=name.toUpperCase();
+// name=name.toLowerCase();
+// alert(name);
+
+// to print the name with first letter  as uppercase and rest lowercase 
+// var name=prompt("enter your name");
+// name=name.toLowerCase();
+// var first=name.slice(0,1).toUpperCase();
+// alert(first+name.slice(1,name.length));
+
+// *+-/% operators 
+// ++ -- += -= operators
+
+// creating a function 
+// function myFunction() {}
+// calling a function
+// myFunction();
+
+// function with a value inside
+// creating a function 
+// function myFunction(bottles) {}
+// calling a function
+// myFunction(bottles);
+
+// return gives output from the function
