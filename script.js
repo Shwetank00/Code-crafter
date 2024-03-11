89b9c0dc64b4cf887ef4177753e3f0fb88093893
Shwetank jain <150837121+Shwetank00@users.noreply.github.com>
2024-03-11 19:54:06 +0530

    basic folder created (#1)
    * basic file created
    
    * added other basic files

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript code
+let firstName = "John";
+
+function greet(name) {
+    alert("Hello, " + name + "!");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code here
+});
